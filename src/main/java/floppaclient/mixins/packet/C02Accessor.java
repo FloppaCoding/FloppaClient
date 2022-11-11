35aa5abe30f98924cbcc7c9f4248642980246742
AtonRay <107542447+AtonRay@users.noreply.github.com>
2022-11-11 23:47:09 +0100

    Added inheritance

--- src/main/java/floppaclient/mixins/packet/C02Accessor.java
+++ src/main/java/floppaclient/mixins/packet/C02Accessor.java
@@ -1,11 +1,13 @@
 package floppaclient.mixins.packet;
 
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.INetHandlerPlayServer;
 import net.minecraft.network.play.client.C02PacketUseEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin({C02PacketUseEntity.class})
-public interface C02Accessor {
+public interface C02Accessor extends Packet<INetHandlerPlayServer> {
     @Accessor
     void setEntityId(int paramInt);
 
