FloppaCoding/FloppaClient

commit e5b95e3357c3a401c963b13035ec345bcc7e3b14

Simplified the ModuleConfig class.

src/main/kotlin/floppaclient/config/ModuleConfig.kt
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder
 import com.google.gson.JsonIOException
 import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
+import floppaclient.FloppaClient.Companion.MOD_NAME
 import floppaclient.config.jsonutils.SettingDeserializer
 import floppaclient.config.jsonutils.SettingSerializer
 import floppaclient.module.ConfigModule
@@ -14,6 +15,13 @@ import java.awt.Color
 import java.io.File
 import java.io.IOException
 
+/**
+ * ## A class to handle the module config file for the mod.
+ *
+ * Provides methods to save and load the settings for all [modules][ModuleManager.modules] in [ModuleManager] to / from the file.
+ *
+ * @author Aton
+ */
 class ModuleConfig(path: File) {
 
     private val gson = GsonBuilder()
@@ -37,10 +45,9 @@ class ModuleConfig(path: File) {
             // create file if it doesn't exist
             configFile.createNewFile()
         } catch (e: Exception) {
-            println("Error initializing module config")
+            println("Error initializing $MOD_NAME module config")
         }
     }
-
 
     fun loadConfig() {
         try {
@@ -64,35 +71,32 @@ class ModuleConfig(path: File) {
                     }
                     if (module.enabled != configModule.enabled) module.toggle()
                     module.keyCode = configModule.keyCode
-                    for (setting in module.settings) {
-                        for (configSetting in configModule.settings) {
-                            // It seems like when the config parsing failed it can result in this being null. The compiler does not know this.
-                            // This check ensures that the rest of the config will still get processed in that case, avoiding the loss of data.
-                            // So just suppress the warning here.
-                            @Suppress("SENSELESS_COMPARISON")
-                            if (configSetting == null) continue
-                            if (setting.name.equals(configSetting.name, ignoreCase = true)) {
-                                when (setting) {
-                                    is BooleanSetting -> setting.enabled = (configSetting as BooleanSetting).enabled
-                                    is NumberSetting -> setting.value = (configSetting as NumberSetting).value
-                                    is ColorSetting -> setting.value = Color((configSetting as NumberSetting).value.toInt(), true)
-                                    is SelectorSetting -> setting.selected = (configSetting as StringSetting).text
-                                    is StringSetting -> setting.text = (configSetting as StringSetting).text
-                                }
-                            }
+                    for (configSetting in configModule.settings) {
+                        // It seems like when the config parsing failed it can result in this being null. The compiler does not know this.
+                        // This check ensures that the rest of the config will still get processed in that case, avoiding the loss of data.
+                        // So just suppress the warning here.
+                        @Suppress("SENSELESS_COMPARISON")
+                        if (configSetting == null) continue
+                        val setting = module.getSettingByName(configModule.name) ?: continue
+                        when (setting) {
+                            is BooleanSetting -> setting.enabled = (configSetting as BooleanSetting).enabled
+                            is NumberSetting -> setting.value = (configSetting as NumberSetting).value
+                            is ColorSetting -> setting.value = Color((configSetting as NumberSetting).value.toInt(), true)
+                            is SelectorSetting -> setting.selected = (configSetting as StringSetting).text
+                            is StringSetting -> setting.text = (configSetting as StringSetting).text
                         }
                     }
                 }
             }
 
         } catch (e: JsonSyntaxException) {
-            println("Error parsing Floppa Client config.")
+            println("Error parsing $MOD_NAME config.")
             println(e.message)
             e.printStackTrace()
         } catch (e: JsonIOException) {
-            println("Error reading Floppa Client config.")
+            println("Error reading $MOD_NAME config.")
         } catch (e: Exception) {
-            println("Floppa Client Config Error.")
+            println("$MOD_NAME Config Error.")
             println(e.message)
             e.printStackTrace()
         }
@@ -104,7 +108,7 @@ class ModuleConfig(path: File) {
                 it.write(gson.toJson(ModuleManager.modules))
             }
         } catch (e: IOException) {
-            println("Error saving Floppa Client config.")
+            println("Error saving $MOD_NAME config.")
         }
     }
 }

src/main/kotlin/floppaclient/module/Module.kt
@@ -6,6 +6,7 @@ import floppaclient.FloppaClient
 import floppaclient.module.settings.Setting
 import floppaclient.utils.ChatUtils
 import net.minecraftforge.common.MinecraftForge
+import org.lwjgl.input.Keyboard
 import kotlin.reflect.full.hasAnnotation
 
 /**
@@ -28,7 +29,7 @@ import kotlin.reflect.full.hasAnnotation
  */
 abstract class Module(
     name: String,
-    keyCode: Int = 0,
+    keyCode: Int = Keyboard.KEY_NONE,
     category: Category = Category.MISC,
     toggled: Boolean = false,
     settings: ArrayList<Setting<*>> = ArrayList(),
@@ -174,12 +175,6 @@ abstract class Module(
     }
 
     fun getSettingByName(name: String): Setting<*>? {
-        for (set in settings) {
-            if (set.name.equals(name, ignoreCase = true)) {
-                return set
-            }
-        }
-        System.err.println("[" + FloppaClient.MOD_NAME + "] Error Setting NOT found: '" + name + "'!")
-        return null
+        return settings.find { it.name.equals(name, ignoreCase = true) }
     }
 }

src/main/kotlin/floppaclient/module/ModuleManager.kt
@@ -166,6 +166,6 @@ object ModuleManager {
     }
 
     fun getModuleByName(name: String): Module? {
-        return modules.stream().filter{module -> module.name.equals(name, ignoreCase = true)}.findFirst().orElse(null)
+        return modules.find{ it.name.equals(name, ignoreCase = true) }
     }
 }

build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
 }
 
 // This variable determine the filename of the produced jar file.
-version = "1.0.3:0.1"
+version = "1.0.3-0.1"
 group = "floppaclient"
 
 repositories {

src/main/kotlin/floppaclient/FloppaClient.kt
@@ -54,7 +54,7 @@ import kotlin.coroutines.EmptyCoroutineContext
 class FloppaClient {
     @Mod.EventHandler
     fun preInit(event: FMLPreInitializationEvent) {
-        runBlocking {
+        scope.launch {
             launch(Dispatchers.IO) {
                 autoactions.loadConfig()
             }

src/main/kotlin/floppaclient/config/AutoActionConfig.kt
@@ -14,6 +14,11 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
+/**
+ * ## A class to handle the auto actions config file for the mod.
+ *
+ * @author Aton
+ */
 class AutoActionConfig(path: File) {
 
     private val gson = GsonBuilder()

src/main/kotlin/floppaclient/config/ExtrasConfig.kt
@@ -13,6 +13,11 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
+/**
+ * ## A class to handle the extras config file for the mod.
+ *
+ * @author Aton
+ */
 class ExtrasConfig(path: File) {
     private val gson = GsonBuilder()
         .registerTypeAdapter(object : TypeToken<MutableSet<BlockPos>>(){}.type, SetBlockPosSerializer())

src/main/kotlin/floppaclient/module/impl/misc/AutoSprint.kt
@@ -5,7 +5,6 @@ import floppaclient.events.PositionUpdateEvent
 import floppaclient.module.Category
 import floppaclient.module.Module
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import org.lwjgl.input.Keyboard
 
 /**
  * Module that makes you always sprint.
@@ -13,7 +12,6 @@ import org.lwjgl.input.Keyboard
  */
 object AutoSprint : Module(
     "Auto Sprint",
-    Keyboard.KEY_O,
     category = Category.MISC,
     description = "A simple auto sprint module that toggles sprinting when moving forwards and not collided " +
             "with anything."

commit 04510d862f59b3ed244a8570a6362e1d28283326

HUD element rework #2
Added a new constructor to the HudElement class, which makes creating hud elemets a lot more streamlined.
It creates the settings for you.

Also fixed a bug introduced in commit e5b95e3357c3a401c963b13035ec345bcc7e3b14
which caused the config not being able to load

src/main/kotlin/floppaclient/config/ModuleConfig.kt
@@ -77,7 +77,7 @@ class ModuleConfig(path: File) {
                         // So just suppress the warning here.
                         @Suppress("SENSELESS_COMPARISON")
                         if (configSetting == null) continue
-                        val setting = module.getSettingByName(configModule.name) ?: continue
+                        val setting = module.getSettingByName(configSetting.name) ?: continue
                         when (setting) {
                             is BooleanSetting -> setting.enabled = (configSetting as BooleanSetting).enabled
                             is NumberSetting -> setting.value = (configSetting as NumberSetting).value

src/main/kotlin/floppaclient/module/Module.kt
@@ -20,7 +20,7 @@ import kotlin.reflect.full.hasAnnotation
  * [modules][ModuleManager.modules] in [ModuleManager].
  * The sample provided below shows how you can do this.
  *
- * ## Adding settings to your module.
+ * ## Adding settings to your Module.
  * If you want your Module to have Settings which appear in the GUI you need to define the settings in your Module and
  * also register them by adding them to the [settings] list.
  *
@@ -58,34 +58,23 @@ import kotlin.reflect.full.hasAnnotation
  * The input transform is used to further limit the range in which the slider can be moved.
  * And the dependency is set so that the setting only shows in the click gui when the corresponding click mode is enabled.
  *
- * ## Adding a HUD to your module
+ * ## Adding a HUD to your Module
  * To add a HUD to your module simply declare an object which inherits from the [HudElement] class within your Module.
  * For this HUD element to be active you need to register it. All you need to do for that is to annotate it with the
  * [RegisterHudElement] annotation.
  * Inside your HUD element object you need to implement the [renderHud][HudElement.renderHud] method.
  * This ís responsible for rendering the element.
  *
- * The following example shows you how to use it.
+ * The following example shows how to use it.
  *
- *     object MyModule : Module(
- *     "My Module",
- *     category = Category.RENDER
- * ) {
- *
- *     private val xHud = +NumberSetting("x", default = 0.0, visibility = Visibility.HIDDEN)
- *     private val yHud = +NumberSetting("y", default = 150.0, visibility = Visibility.HIDDEN)
- *     private val scaleHud = +NumberSetting("scale", 1.0, 0.1, 4.0, 0.01, Visibility.HIDDEN)
- *
- *     @RegisterHudElement
- *     object DayCounter : HudElement(xHud, yHud,
- *         mc.fontRendererObj.getStringWidth("Day (32)"),
- *         mc.fontRendererObj.FONT_HEIGHT * 2 + 1, scaleHud
- *     ) {
- *         override fun renderHud() {
- *             // Rendering implementation
+ *     object MyModule : Module("My Module", category = Category.RENDER) {
+ *         @RegisterHudElement
+ *         object MyHudElement : HudElement(this, 0, 150, 100, 20) {
+ *             override fun renderHud() {
+ *                 // Rendering implementation
+ *             }
  *         }
  *     }
- * }
  *
  *
  * @author Aton

src/main/kotlin/floppaclient/module/ModuleManager.kt
@@ -14,20 +14,20 @@ import floppaclient.ui.hud.EditHudGUI
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 /**
- * ## This object handles all the modules of the mod.
+ * # This object handles all the modules of the mod.
  *
  * After making a [Module] it just has to be added to the [modules] list and
  * everything else will be taken care of automatically. This entails:
  *
- * It will be added to the click gui in the order it is put in here. But keep in mind that the category is set within
+ * + It will be added to the click gui in the order it is put in here. But keep in mind that the category is set within
  * the module. The comments here are only for readability.
  *
- * All settings that are registered within the module will be saved to and loaded from the module config.
+ * + All settings that are registered within the module will be saved to and loaded from the module config.
  * For this to properly work remember to register the settings to the module.
  *
- * The module will be registered and unregistered to the forge eventbus when it is enabled / disabled.
+ * + The module will be registered and unregistered to the forge eventbus when it is enabled / disabled.
  *
- * The module will be informed of its keybind presses.
+ * + The module will be informed of its keybind presses.
  *
  *
  * @author Aton

src/main/kotlin/floppaclient/module/impl/render/CoordinateDisplay.kt
@@ -4,9 +4,7 @@ import floppaclient.FloppaClient.Companion.mc
 import floppaclient.module.Category
 import floppaclient.module.Module
 import floppaclient.module.RegisterHudElement
-import floppaclient.module.settings.Visibility
 import floppaclient.module.settings.impl.BooleanSetting
-import floppaclient.module.settings.impl.NumberSetting
 import floppaclient.ui.hud.HudElement
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.MovingObjectPosition
@@ -22,28 +20,12 @@ object CoordinateDisplay : Module(
     category = Category.RENDER,
     description = "Renders your coordinates on your screen."
 ) {
-    private val showLookingAt = BooleanSetting("Looking At", false, description = "Displays the coordinates of the block you are looking at in a second line.")
-
-    private val xHud = NumberSetting("x", default = 0.0, visibility = Visibility.HIDDEN)
-    private val yHud = NumberSetting("y", default = 150.0, visibility = Visibility.HIDDEN)
-    private val scaleHud = NumberSetting("scale",1.0,0.1,4.0, 0.01, visibility = Visibility.HIDDEN)
-
-    init {
-        this.addSettings(
-            showLookingAt,
-            xHud,
-            yHud,
-            scaleHud
-        )
-    }
+    private val showLookingAt by BooleanSetting("Looking At", false, description = "Displays the coordinates of the block you are looking at in a second line.")
 
     @RegisterHudElement
-    object CoordinateHUD : HudElement(
-        xHud,
-        yHud,
+    object CoordinateHUD : HudElement(this, 0, 150,
         mc.fontRendererObj.getStringWidth("123 / 12 / 123 (12.3 / 12.3)"),
         mc.fontRendererObj.FONT_HEIGHT * 2 + 1,
-        scaleHud
     ) {
         override fun renderHud() {
 
@@ -60,7 +42,7 @@ object CoordinateDisplay : Module(
             mc.fontRendererObj.drawString(coordText, 0, 0, 0xffffff)
 
             // handle looking at
-            if (showLookingAt.enabled) {
+            if (showLookingAt) {
                 val la = mc.objectMouseOver
 
                 if (la != null && la.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
@@ -72,5 +54,4 @@ object CoordinateDisplay : Module(
             this.width = mc.fontRendererObj.getStringWidth(coordText)
         }
     }
-
 }

src/main/kotlin/floppaclient/ui/hud/EditHudGUI.kt
@@ -1,5 +1,6 @@
 package floppaclient.ui.hud
 
+import floppaclient.FloppaClient
 import floppaclient.floppamap.dungeon.MapRender
 import floppaclient.ui.clickgui.util.ColorUtil
 import floppaclient.ui.clickgui.util.FontUtil
@@ -136,6 +137,10 @@ object EditHudGUI : GuiScreen() {
     override fun mouseReleased(mouseX: Int, mouseY: Int, state: Int) {
         draggingElement = null
         super.mouseReleased(mouseX, mouseY, state)
+    }
+
+    override fun onGuiClosed() {
+        FloppaClient.moduleConfig.saveConfig()
     }
 
     private fun isCursorOnElement(mouseX: Int, mouseY: Int, element: HudElement): Boolean {

src/main/kotlin/floppaclient/ui/hud/HudElement.kt
@@ -1,8 +1,9 @@
 package floppaclient.ui.hud
 
-import floppaclient.utils.render.HUDRenderUtils
+import floppaclient.module.Module
 import floppaclient.module.settings.Visibility
 import floppaclient.module.settings.impl.NumberSetting
+import floppaclient.utils.render.HUDRenderUtils
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraftforge.client.event.RenderGameOverlayEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -12,13 +13,14 @@ import java.awt.Color
  * Provides functionality for game overlay elements.
  * @author Aton
  */
-abstract class HudElement(
-    private val xSett: NumberSetting,
-    private val ySett: NumberSetting,
-    var width: Int = 10,
-    var height: Int = 10,
-    val scale: NumberSetting = NumberSetting("scale",1.0,0.1,4.0, 0.02, visibility = Visibility.HIDDEN),
-) {
+abstract class HudElement{
+
+    private val xSett: NumberSetting
+    private val ySett: NumberSetting
+    val scale: NumberSetting
+
+    var width: Int
+    var height: Int
 
     private val zoomIncrement = 0.05
 
@@ -36,6 +38,39 @@ abstract class HudElement(
         set(value) {
             ySett.value = value.toDouble()
         }
+
+    /**
+     * Sets up a hud Element.
+     * This constructor takes care of creating the [NumberSetting]s required to save the position and scale of the hud
+     * element to the config.
+     */
+    constructor(module: Module, xDefault: Int = 0, yDefault: Int = 0, width: Int = 10, height: Int = 10, defaultScale: Double = 1.0) {
+        val id = module.settings.count { it.name.startsWith("xHud") }
+        val xHud = NumberSetting("xHud_$id", default = xDefault.toDouble(), visibility = Visibility.HIDDEN)
+        val yHud = NumberSetting("yHud_$id", default = yDefault.toDouble(), visibility = Visibility.HIDDEN)
+        val scaleHud = NumberSetting("scaleHud_$id",defaultScale,0.1,4.0, 0.01, visibility = Visibility.HIDDEN)
+
+        module.addSettings(xHud, yHud, scaleHud)
+
+        this.xSett = xHud
+        this.ySett = yHud
+        this.scale = scaleHud
+
+        this.width = width
+        this.height = height
+    }
+
+    /**
+     * It is advised to use the other constructor unless this one is required.
+     */
+    constructor(xHud: NumberSetting, yHud: NumberSetting, width: Int = 10, height: Int = 10, scale: NumberSetting) {
+        this.xSett = xHud
+        this.ySett = yHud
+        this.scale = scale
+
+        this.width = width
+        this.height = height
+    }
 
     /**
      * Resets the position of this hud element by setting the value of xSett and ySett to their default.
@@ -98,5 +133,4 @@ abstract class HudElement(
 
         GlStateManager.popMatrix()
     }
-
 }